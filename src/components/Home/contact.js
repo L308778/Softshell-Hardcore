7bfe078374fdf5a9cc2558aba3439460c302be25
Leonard <leonard.platzer@gmail.com>
2022-01-08 14:56:55 +0100

    Changed font and colour

--- src/components/Home/contact.js
+++ src/components/Home/contact.js
@@ -7,13 +7,13 @@ export default function Contact() {
   return (
       <div className="Contact-container">
         <a href="https://www.instagram.com/softshellhardcore/" target="_blank">
-                  <AiFillInstagram size={40} style={{margin:"20px", color:"rgb(226, 108, 206)"}}/>
+                  <AiFillInstagram size={40} style={{margin:"20px", color:"rgb(239, 129, 159)"}}/>
                   </a>
                   <p className="Contact-email">
                   antiniacreation@gmail.com
               </p>
               <a href="mailto:antiniacreation@gmail.com">
-                  <AiFillMail size={40} style={{margin:"20px", color:"rgb(226, 108, 206)"}}/>
+                  <AiFillMail size={40} style={{margin:"20px", color:"rgb(239, 129, 159)"}}/>
                   </a>
     </div>
   );
